b32141fe394860994fcb630ff02faab370501b04
Logan Grant <>
2021-04-07 23:13:59 +0000

    fsthrfsthsrthrhrth

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('button1').addEventListener('click', game)
+
+function game () {
+  document.getElementById('display').innerHTML = '<iframe class="p51"src="https://editor.p5js.org/ILoganGrantI/embed/ptAPl7Tnzd">'
+}
