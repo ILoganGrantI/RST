86ee16a80ae1ef9d1a7be0621d9640f2c090a459
Logan Grant <>
2021-04-08 13:49:59 +0000

    --

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('button1').addEventListener('click', game)
-
-function game () {
-  document.getElementById('display').innerHTML = '<iframe class="p51"src="https://editor.p5js.org/ILoganGrantI/embed/ptAPl7Tnzd">'
-}
